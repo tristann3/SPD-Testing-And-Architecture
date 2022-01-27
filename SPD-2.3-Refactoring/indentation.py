52a1bb9967507ea44ad5caf0a8a82d169d307b1b
tristann3 <tristan15thompson@gmail.com>
2022-01-27 14:28:35 -0500

    changed url to ignore max 79 line length

--- SPD-2.3-Refactoring/indentation.py
+++ SPD-2.3-Refactoring/indentation.py
@@ -14,9 +14,7 @@ def set_person_info(
     # ...    
     write_to_db()
 
-photo_path = \
-        "https://en.wikipedia.org/wiki/Tim_Hunt#/media/File:\
-        Tim_Hunt_at_UCSF_05_2009_(4).jpg"
+url = "https://en.wikipedia.org/wiki/Tim_Hunt#/media/File:Tim_Hunt_at_UCSF_05_2009_(4).jpg"
         
 set_person_info(
     'Tim', 'Hunt', 'male','19 February 1943', photo_path, 'Uited Kingdom',
