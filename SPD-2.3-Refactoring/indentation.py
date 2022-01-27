ee74d63ae2217490c5562f5d4bbe330d61923bb3
tristann3 <tristan15thompson@gmail.com>
2022-01-27 14:06:11 -0500

    fixed max line length using line continuation

--- SPD-2.3-Refactoring/indentation.py
+++ SPD-2.3-Refactoring/indentation.py
@@ -4,7 +4,9 @@ def write_to_db():
    print('person info are written into db.')
     
     
-def set_person_info(first_name, last_name, gender, date_of_birth, photo, nationality, place_of_birth): 
+def set_person_info(
+    first_name, last_name, gender, date_of_birth, photo, nationality,
+    place_of_birth): 
     if not first_name:
         print('first name is empty.') 
     if not last_name:
@@ -12,5 +14,10 @@ def set_person_info(first_name, last_name, gender, date_of_birth, photo, nationa
     # ...    
     write_to_db()
 
-photo_path = "https://en.wikipedia.org/wiki/Tim_Hunt#/media/File:Tim_Hunt_at_UCSF_05_2009_(4).jpg"
-set_person_info('Tim', 'Hunt', 'male','19 February 1943', photo_path, 'Uited Kingdom', 'Neston, Cheshire, England')
+photo_path = \
+        "https://en.wikipedia.org/wiki/Tim_Hunt#/media/File:\
+        Tim_Hunt_at_UCSF_05_2009_(4).jpg"
+        
+set_person_info(
+    'Tim', 'Hunt', 'male','19 February 1943', photo_path, 'Uited Kingdom',
+    'Neston, Cheshire, England')
