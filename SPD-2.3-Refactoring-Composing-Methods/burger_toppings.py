8006102c7521ecd9fe5929f09d02dd2217c860e6
Tristan Thompson <tristan15thompson@gmail.com>
2022-02-03 13:36:14 -0500

    refactored burger_toppings

--- SPD-2.3-Refactoring-Composing-Methods/burger_toppings.py
+++ SPD-2.3-Refactoring-Composing-Methods/burger_toppings.py
@@ -6,12 +6,12 @@ pickle = 20 # [gr]
 tomatoes = 25 # [gr]
 lettuce = 15 # [gr]
 buns = 95 # [gr]
-sandwich_weight = (2 * patty + 4 * pickle + 3 * tomatoes + 2 * lettuce
+ny_weight = (2 * patty + 4 * pickle + 3 * tomatoes + 2 * lettuce
                 + 2 * buns)
-print("NY Burger Weight", sandwich_weight)
+print("NY Burger Weight", ny_weight)
 kimchi = 30 # [gr]
 mayo = 5 # [gr]
 golden_fried_onion = 20 # [gr]
-sandwich_weight = (2 * patty + 4 * pickle + 3 * tomatoes 
+seoul_kimchi_weight = (2 * patty + 4 * pickle + 3 * tomatoes 
                 + kimchi + mayo + golden_fried_onion + 2 * buns)
-print("Seoul Kimchi Burger Weight", sandwich_weight)
+print("Seoul Kimchi Burger Weight", seoul_kimchi_weight)
