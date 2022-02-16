b019b76b947a2fe7ee01d2d1605129e928d18838
Tristan Thompson <tristan15thompson@gmail.com>
2022-02-15 19:55:09 -0500

    refactored read_file.py

---
+++ SPD-2.3-Simplify-Conditionals/read_file.py
@@ -0,0 +1,12 @@
+# by Kami Bigdely
+# Remove control flag
+# Reference: https://stackoverflow.com/a/10140333/81306
+# This code snippet reads up to the end of the file
+n = 16
+with open('foobar.file', 'rb') as fp:
+    chunk = fp.read(n)
+    if chunk == '': # end of file, stop running.
+        pass
+    else:
+        print(chunk)
+    # process(chunk)
