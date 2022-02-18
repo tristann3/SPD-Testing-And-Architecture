f287b5572c77c5fcf59390238b3db2248e0fd892
tristann3 <tristan15thompson@gmail.com>
2022-02-17 20:51:01 -0500

    added string, NoneType, non-string and expected unit tests

--- unit-testing/extract_position.py
+++ unit-testing/extract_position.py
@@ -1,5 +1,5 @@
 def extract_position(line):
-    if not line:
+    if not isinstance(line, str):
         pos = None
     else:
         if 'debug' in line or 'error' in line:
@@ -11,6 +11,22 @@ def extract_position(line):
             else:
                 pos = None
     return pos
+
+def test_extract_position_debug():
+    line = 'debug'
+    assert extract_position(line) == None
+
+def test_extract_position_hello_world():
+    line = 'x:hello_world'
+    assert extract_position(line) == 'hello_world'
+
+def test_extract_position_none():
+    line = None
+    assert extract_position(line) == None
+
+def test_extract_position_not_string_type():
+    line = 1
+    assert extract_position(line) == None
 
 if __name__ == "__main__":
     result1 = extract_position('|error| numerical calculations could not converge.')
