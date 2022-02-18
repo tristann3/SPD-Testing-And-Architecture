1403911a77fc724ca96cf73c70a97a3ac82914f2
tristann3 <tristan15thompson@gmail.com>
2022-02-17 19:10:24 -0500

    wrote zero, negative and expected unit tests for carbon_dating.py

--- unit-testing/carbon_dating.py
+++ unit-testing/carbon_dating.py
@@ -9,10 +9,27 @@ DECAY_CONSTANT = -0.693
     # - What if the input is zero or negative?
     # - Add the necessary logic to make sure the function handle every possible input properly. Then write a unit test against this special case.
 
+def test_get_age_carbon_14_dating():
+    ratio = 0.35
+
+    assert math.isclose(8680.34, get_age_carbon_14_dating(ratio), abs_tol = 0.01)
+
+def test_get_age_carbon_14_dating_zero():
+    ratio = 0.0
+
+    assert math.isclose(0, get_age_carbon_14_dating(ratio), abs_tol = 0.01)
+
+def test_get_age_carbon_14_dating_negative():
+    ratio = -1.1
+
+    assert math.isclose(0, get_age_carbon_14_dating(ratio), abs_tol = 0.01)
+
 def get_age_carbon_14_dating(carbon_14_ratio):
     """Returns the estimated age of the sample in year.
     carbon_14_ratio: the percent (0 < percent < 1) of carbon-14
     in the sample conpared to the amount in living
     tissue (unitless).
     """
+    if carbon_14_ratio <= 0:
+        raise TypeError("carbon 14 ratio must be greater than 0")
     return math.log(carbon_14_ratio) / DECAY_CONSTANT * T_HALF
